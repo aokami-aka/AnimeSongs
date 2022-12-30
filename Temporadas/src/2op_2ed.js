58e6fb0c6f6cabf71813191e646f6f7ddd17d927
Victor Santos <61319771+aokami-aka@users.noreply.github.com>
2022-12-30 13:47:58 -0300

    Add files via upload

---
+++ Temporadas/src/2op_2ed.js
@@ -0,0 +1,103 @@
+function ShowHiddenop1() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+
+	if(document.getElementById('op1').style.display == 'block')
+		document.getElementById('op1').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+	else
+		document.getElementById('op1').style.display = 'block',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+}
+
+function ShowHiddenop2() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+
+	if(document.getElementById('op2').style.display == 'block')
+		document.getElementById('op1').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'block', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+}
+
+function ShowHiddened1() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+
+	if(document.getElementById('ed1').style.display == 'block')
+		document.getElementById('op1').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'block',
+		document.getElementById('ed2').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+}
+
+function ShowHiddened2() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+
+	if(document.getElementById('ed2').style.display == 'block')
+		document.getElementById('op1').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'block',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause();
+}
