df929d37a2171250494aed9b55bb9a9bdf0b881d
Victor Santos <61319771+aokami-aka@users.noreply.github.com>
2023-01-09 13:36:12 -0300

    Add files via upload

---
+++ Temporadas/src/2op_5ed.js
@@ -0,0 +1,293 @@
+function ShowHiddenop1() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+	const video5 = document.getElementById('video5');
+	const video6 = document.getElementById('video6');
+	const video7 = document.getElementById('video7');
+
+	if(document.getElementById('op1').style.display == 'block')
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+	else
+		document.getElementById('op1').style.display = 'block',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+}
+
+function ShowHiddenop2() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+	const video5 = document.getElementById('video5');
+	const video6 = document.getElementById('video6');
+	const video7 = document.getElementById('video7');
+
+	if(document.getElementById('op2').style.display == 'block')
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'block', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+}
+
+function ShowHiddened1() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+	const video5 = document.getElementById('video5');
+	const video6 = document.getElementById('video6');
+	const video7 = document.getElementById('video7');
+
+	if(document.getElementById('ed1').style.display == 'block')
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'block',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+}
+
+function ShowHiddened2() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+	const video5 = document.getElementById('video5');
+	const video6 = document.getElementById('video6');
+	const video7 = document.getElementById('video7');
+
+	if(document.getElementById('ed2').style.display == 'block')
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'block',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+}
+
+function ShowHiddened3() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+	const video5 = document.getElementById('video5');
+	const video6 = document.getElementById('video6');
+	const video7 = document.getElementById('video7');
+
+	if(document.getElementById('ed3').style.display == 'block')
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'block',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+}
+
+function ShowHiddened4() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+	const video5 = document.getElementById('video5');
+	const video6 = document.getElementById('video6');
+	const video7 = document.getElementById('video7');
+
+	if(document.getElementById('ed4').style.display == 'block')
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'block',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+}
+
+function ShowHiddened5() {
+	const video1 = document.getElementById('video1');
+	const video2 = document.getElementById('video2');
+	const video3 = document.getElementById('video3');
+	const video4 = document.getElementById('video4');
+	const video5 = document.getElementById('video5');
+	const video6 = document.getElementById('video6');
+	const video7 = document.getElementById('video7');
+
+	if(document.getElementById('ed5').style.display == 'block')
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'none',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+	else
+		document.getElementById('op1').style.display = 'none',
+		document.getElementById('op2').style.display = 'none', 
+		document.getElementById('ed1').style.display = 'none',
+		document.getElementById('ed2').style.display = 'none',
+		document.getElementById('ed3').style.display = 'none',
+		document.getElementById('ed4').style.display = 'none',
+		document.getElementById('ed5').style.display = 'block',
+		video1.pause(),
+		video2.pause(),
+		video3.pause(),
+		video4.pause(),
+		video5.pause(),
+		video6.pause(),
+		video7.pause();
+}
